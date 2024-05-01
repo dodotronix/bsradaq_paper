b9a1ed1b7647a1eccc49b0b75391f6dd8e269064
dodotronix <dodotronix@gmail.com>
2024-05-01 14:54:25 +0200

    normalized values and changed color of the plots

--- plots/filters.py
+++ plots/filters.py
@@ -18,10 +18,13 @@ dir_path = os.path.dirname(sys.argv[1])
 a = np.loadtxt(os.path.join(dir_path, 'timber_data/vfc_abort_gap_raw_data_spill.txt'), 
                encoding='ASCII')
 
+a = a/max(a) # normalize data
+
 data = np.tile(a, (8, 1))
 
 X = np.arange(data.shape[1])
 Y = np.arange(data.shape[0])
+
 X, Y = np.meshgrid(X, Y)
 
 fig = plt.figure()
@@ -31,8 +34,10 @@ ax.tick_params(axis='y',which='major',length=data.shape[0], width=1, labelsize=1
 # ax.set_yticks([0, len(Y)-1, 1])
 # ax.set_xticks([0, len(X)-1, 1])
 
-surf = ax.plot_wireframe(X, Y, data, rstride=1, cstride=0,
-                   cmap='Oranges', edgecolor=None, linewidth=2, antialiased=True)
+surf = ax.plot_wireframe(X, Y, data, 
+                         rstride=1, cstride=0, color='red', 
+                         linewidth=2, antialiased=True)
+
 
 plt.xticks(np.arange(0, 64, 9))
 plt.yticks(np.arange(0, 8, 1))
