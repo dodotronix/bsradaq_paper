376544ac55587b0cd6e016deed9dc693587a1c01
dodotronix <dodotronix@gmail.com>
2024-05-08 18:31:22 +0200

    changed names and size of fonts in the ions plot

--- plots/ions.py
+++ plots/ions.py
@@ -30,14 +30,14 @@ plt.plot(agm_current_norm, '-x', markersize=4, color=(0.878, 0.106, 0.141), line
 plt.plot(agm_new_norm, '-o', markersize=2, color=(0, 0.3, 0.7), linewidth=0.8)
 
 ax.tick_params(axis='both', direction='in', labelcolor='k',
-               labelsize='small', width=0.6, which='both',
+               labelsize='x-large', width=0.6, which='both',
                top=True, right=True)
 
 plt.xticks(np.arange(0, 64, 9))
 
 
-plt.ylabel('Amplitude [-]')
-plt.xlabel('Output filter index [-]')
+plt.ylabel('Amplitude [-]', fontsize="x-large")
+plt.xlabel('Output filter index [-]', fontsize="x-large")
 plt.xlim([0, 63])
 plt.grid(color=(0.7, 0.7, 0.7), linestyle='--', linewidth=0.6)
 
@@ -45,16 +45,16 @@ ax2 = ax.twinx()
 ax2.plot(agm_error, '-o', markersize=5, color=(0.961, 0.761, 0.067), linewidth=0.8)  
 
 ax2.tick_params(axis='both', direction='in', labelcolor='k',
-               labelsize='small', width=0.6, which='both',
+               labelsize='x-large', width=0.6, which='both',
                top=True, right=True)
 
-ax2.set_ylabel('deviation [\%]')
+ax2.set_ylabel('Relative Error [\%]', fontsize="x-large")
 ax2.set_yticks(np.linspace(-8, 6, 8))
 ax2.set_ylim([-6.71, 4.51])
 
-legend = fig.legend(['Current AGM', 'New AGM', 'Deviation'], 
+legend = fig.legend(['Operational AGM', 'New AGM', 'Relative Error'], 
          loc='upper left', shadow=False, frameon=True, borderpad=0.5,
-         framealpha=1, facecolor='white', fancybox=False, edgecolor=(0, 0, 0),
+         framealpha=1, fontsize="x-large", facecolor='white', fancybox=False, edgecolor=(0, 0, 0),
                     bbox_to_anchor=(0,1), bbox_transform=ax.transAxes)
 
 frame = legend.get_frame()
